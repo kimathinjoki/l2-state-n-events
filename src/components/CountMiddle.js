44bde960259e8b66d693b847f4f10db0f34dc722
kimathinjoki <dfkimathinjoki@gmail.com>
2023-01-11 03:52:37 -0800

    add click

--- src/components/CountMiddle.js
+++ src/components/CountMiddle.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 
 function CountMiddle(){
 
+    const [count, setCount]=useState(0)
+
+    function clicked(){
+        setCount(count +1)
+    }
+
     return(
             <div>
                 <center>
-                    <h4 className="btn-text">0</h4>
-                    <button className="timeButton">CLICK ME</button>
+                    <h4 className="btn-text">{count}</h4>
+                    <button className="timeButton" onClick={clicked}>CLICK ME</button>
                 </center>
             </div>
     )
--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      {/* <CountUp/>
-      <CountDown/>
-      <CountMiddle/> */}
+      {/*<CountUp/>*/
+      <CountMiddle/>}
   </React.StrictMode>
 );
 
